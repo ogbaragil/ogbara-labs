8d56239258ff7d16837da29a8c012eae1017b09b
ogbaragil <ogbaragil@gmail.com>
2026-06-07 00:34:14 +1000

    Add files via upload

--- lifegrid/sw.js
+++ lifegrid/sw.js
@@ -1,5 +1,5 @@
 /* Life Grid — your life in months service worker */
-const CACHE = "lifegrid-v20";
+const CACHE = "lifegrid-v21";
 const ASSETS = ["./","./index.html","./manifest.webmanifest","./supabase-config.js","./cloud.js",
   "./icons/icon-192.png","./icons/icon-512.png","./icons/maskable-512.png",
   "./icons/apple-touch-icon.png","./icons/favicon-32.png"];
